8e0204e7373da60fe01a2e724edb1b8d5352b1df
s1971114 <yuito5123@icloud.com>
2020-10-23 16:34:40 +0900

    人数と名前を入力するプログラム

---
+++ app/src/main/java/com/example/onenightjinrou/ui/NumberOfPeopleName.java
@@ -0,0 +1,36 @@
+package com.example.onenightjinrou.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.onenightjinrou.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class NumberOfPeopleName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_of_people_name);
+
+        public static void main(String[]Object args){
+            String result[] = new String[10];
+            Scanner sc = new Scanner(System.in);
+            System.out.println("人数を決めてください");
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            for(int i = 0; i  < num; i++){
+                result[i] = sc.next();
+            }
+        }
+    }
+}
+
+
+
+
+
